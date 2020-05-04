be963395593c11497ddf13e8d34838438802aebc
rbeatie <rbeatie@CM0R0D91A2F3C4GREX5.local>
2020-05-04 11:20:47 -0700

    initial commit for typescript build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
+import * as FlexWebChat from '@twilio/flex-webchat-ui';
+import { useEffect } from 'react';
 import './App.css';
 
+//  change the your AccountSid
+const accountSid = 'AC#####';
+
+// change to your Flex Flow SID
+const flexFlowSid = 'FO######';
+
+const appConfig: any = {
+  accountSid,
+  flexFlowSid,
+  logLevel: 'error',
+  startEngagementOnInit: true,
+  disableLocalStorage: false
+};
+
 function App() {
+
+  useEffect(() => {
+      FlexWebChat.createWebChat(appConfig).then((webchat: FlexWebChat.FlexWebChat) => {
+
+        const {manager} = webchat;
+
+        //Posting question from preengagement form as users first chat message
+        FlexWebChat.Actions.on("afterStartEngagement", (payload) => {
+          const {question} = payload.formData;
+
+          if (!question) return;
+
+          const {channelSid} = manager.store.getState().flex.session;
+
+          manager
+              .chatClient.getChannelBySid(channelSid)
+              .then(channel => channel.sendMessage(question));
+        });
+
+        // Changing the Welcome message
+        manager.strings.WelcomeMessage = "New text for welcome message";
+
+        // Render WebChat
+        webchat.init();
+      });
+
+  });
+
   return (
     <div className="App">
       <header className="App-header">
